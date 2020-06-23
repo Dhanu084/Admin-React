ad997388cab15357639b40008447be89f143c1af
Dhanush Kiran K <dhanuu08@gmail.com>
2020-06-23 16:09:43 +0530

    testing catalogues db

--- src/components/Catalogues.js
+++ src/components/Catalogues.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default class Catalogues extends Component {
+export class Catalogues extends Component {
   render() {
+    console.log("Catlogues", this.props);
     return <div>Catalogues</div>;
   }
 }
+function mapStateToProps(state) {
+  return {
+    catalogues: state.catalogues,
+  };
+}
+export default connect(mapStateToProps)(Catalogues);
